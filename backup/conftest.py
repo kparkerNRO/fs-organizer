2e69439655c30e57a5b413919a67c1fae44fbbd2
KP <kp@newrobotorder.com>
2025-01-07 09:05:35 -0500

    remove backup code

--- backup/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def test_file(tmp_path):
-    file_path = tmp_path / "testfile.txt"
-    file_path.write_text("This is a test file")
-    yield file_path
--- backup/database/sqlite.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy import create_engine
-
-RUN_DB = create_engine('sqlite:///data/run.db')
